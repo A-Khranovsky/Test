0d241fd2ce5443baede28bf2b7afca003a848fd6
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2022-01-14 12:56:37 +0200

    Created seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $users = User::factory(2)->create();
+        $tasks = Task::factory()->count(5)->make(['user_id' => null]);
+
+        $tasks->each(function (Task $advt) use ($users) {
+            $advt->user()->associate($users->random());
+            $advt->save();
+        });
     }
 }
