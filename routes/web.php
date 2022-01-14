a0f45cb4a01c6c28d1aa583972472246dcfd33b6
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2022-01-14 13:08:22 +0200

    Created routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TaskController::class, 'index'])->name('home');
+Route::get('/tasks', [TaskController::class, 'userTasks'])->name('userTasks')->middleware('auth');
+Route::get('/callback', [TaskController::class, 'callback']);
+Route::get('/logout', [TaskController::class, 'logout'])->name('logout');
